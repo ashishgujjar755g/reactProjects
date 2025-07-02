50b88864aa12aac66250ebfcfc1d4828a95c55fb
Ashish Gujjar <ashishgujjar755g@gmail.com>
2025-07-02 09:06:02 +0530

    context api disscussion

---
+++ ContextApi/src/components/Profile.jsx
@@ -0,0 +1,11 @@
+import React, { useContext } from 'react'
+import UserContext from '../context/UserContext'
+import UserContextProvider from '../context/UserContexProvider'
+
+function Profile() {
+      const {user} = useContext(UserContext)
+      if(!user) return <div>please ogin</div>
+      return <div>Welcome  {user.username}</div>
+}
+
+export default Profile
