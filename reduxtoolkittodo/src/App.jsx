e9988a095cde3f4297a561322942878ad3879721
Ashish Gujjar <ashishgujjar755g@gmail.com>
2025-07-14 19:18:46 +0530

    working lazy day

--- reduxtoolkittodo/src/App.jsx
+++ reduxtoolkittodo/src/App.jsx
@@ -6,7 +6,7 @@ import './App.css'
 function App() {
   const [count, setCount] = useState(0)
 
-  return <>fsdfstytftftfyftydfgdfgdfgdfrgfdgfddfvdfggdfgggit</>;
+  return <>fsdfstytftftfyftydfgdfgdfgdfrgfdgfddfvdfggdfgggitdfddfdd</>;
 }
 
 export default App
