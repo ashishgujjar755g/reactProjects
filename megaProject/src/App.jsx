2a9288c032f3b666b99d3ebafecb50f36dac54c3
Ashish Gujjar <ashishgujjar755g@gmail.com>
2025-07-19 17:52:25 +0530

    setup environment variable for appwrite

--- megaProject/src/App.jsx
+++ megaProject/src/App.jsx
@@ -1,12 +1,13 @@
-import { useState } from 'react'
+
 
 
 function App() {
-  const [count, setCount] = useState(0)
+    console.log(import.meta.env.VITE_APPWRITE_URL);
 
   return (
     <>
-      
+  
+    
     </>
   )
 }
