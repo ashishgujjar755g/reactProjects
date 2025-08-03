36ba40118ff72ae5f579103cf9292b9b5ae1be84
Ashish Gujjar <ashishgujjar755g@gmail.com>
2025-08-03 18:08:40 +0530

    adding comments in database service

--- megaProject/src/appwrite/config.js
+++ megaProject/src/appwrite/config.js
@@ -1,90 +1,124 @@
-import { Client , ID ,Storage ,Databases ,Query } from "appwrite";
-import conf from '../conf/conf'
+import { Client, ID, Storage, Databases, Query } from "appwrite";
+import conf from "../conf/conf";
 
+//making class for good practicing and experienced code it makes re-usable code
 export class Service {
+  client = new Client(); //here we create new object for accessing Client because it is store in class from in appwrite
 
-      client = new Client ();
-      database;
-      bucket;
-      constructor() {
-          this.client
-              .setEndpoint(conf.appwriteUrl)
-              .setProject(conf.appwriteProjectId);
+  database; //same as upper
 
-          this.database = new Databases(this.client);
-          this.bucket = new Storage(this.client);    
-      }
-      async createPost ({title,slug,content,featureImage,status,userId}){
+  bucket; //same as upper
+  constructor() {
+    //jab bhi service object create hoga constructor call hoga
 
-            try {
-                  return await this.database.createDocument(conf.appwriteDatabaseId,conf.appwriteCollectionId,slug,{title,content,featureImage,status,userId})
-            } catch (error) {
-                  console.log("AppWrite Service :: createPost :: ",error);
-                  
-            }
-      }
+    this.client
+      .setEndpoint(conf.appwriteUrl)
+      .setProject(conf.appwriteProjectId);
 
-      async updatePost (slug,{title,content,featureImage,status}){
-            try {
-                  return await this.database.updateDocument(conf.appwriteDatabaseId,conf.appwriteCollectionId,slug,{title,content,featureImage,status})
-            } catch (error) {
-                  console.log("AppWrite Service :: updatePost :: ",error);
-                  
-            }
-      }
+    this.database = new Databases(this.client); //here we make object same as client but here we are making databases using upper client
 
-      async deletePost (slug) {
-            try {
-                   await this.database.deleteDocument(conf.appwriteDatabaseId,conf.appwriteCollectionId,slug)
-                   return true
-            } catch (error) {
-                  console.log("AppWrite Service :: deltePost :: ",error);
-                  return false
-                  
-            }
-      }
+    this.bucket = new Storage(this.client); //same as upper
+  }
 
-      async getPost(slug){
-            try {
-                  return await this.database.getDocument(conf.appwriteDatabaseId,conf.appwriteCollectionId,slug)
-            } catch (error) {
-                  console.log("AppWrite Service :: getPost ::",error);
-                  return false
-                  
-            }
-      }
-      async getPosts(queries = [Query.equal("status","active")]){
-            try {
-                  return await this.database.listDocuments(conf.appwriteDatabaseId,conf.appwriteCollectionId,queries)
-            } catch (error) {
-                  console.log("AppWrite Service :: getPosts ::",error);
-                  
-            }
-      }
-        //file upload service
-      async uploadFile(file) {
-            try {
-                  return await this.bucket.createFile(conf.appwriteBucketId, ID.unique(), file);
-            } catch (error) {
-                  console.log("AppWrite Service :: uploadFile ::", error);
-                  return false;
-            }
-      }
-     //file delete service
-      async deleteFile(fileId) {
-            try {
-                  return await this.bucket.deleteFile(conf.appwriteBucketId, fileId);
-                  return true;
-            } catch (error) {
-                  console.log("AppWrite Service :: deleteFile ::", error);
-                  return false;
-            }
-      }
+  //make service for createPost
+  async createPost({ title, slug, content, featureImage, status, userId }) {
+    try {
+      return await this.database.createDocument(
+        conf.appwriteDatabaseId,
+        conf.appwriteCollectionId,
+        slug,
+        { title, content, featureImage, status, userId }
+      );
+    } catch (error) {
+      console.log("AppWrite Service :: createPost :: ", error);
+    }
+  }
 
-      getFilePreview(fileId) {
-            return this.bucket.getFilePreview(conf.appwriteBucketId, fileId);
-      }
+  //make service for updatePost and here slug is used as a unique key
+  async updatePost(slug, { title, content, featureImage, status }) {
+    try {
+      return await this.database.updateDocument(
+        conf.appwriteDatabaseId,
+        conf.appwriteCollectionId,
+        slug,
+        { title, content, featureImage, status }
+      );
+    } catch (error) {
+      console.log("AppWrite Service :: updatePost :: ", error);
+    }
+  }
 
+  //make service for deletePost and slug is key
+  async deletePost(slug) {
+    try {
+      await this.database.deleteDocument(
+        conf.appwriteDatabaseId,
+        conf.appwriteCollectionId,
+        slug
+      );
+      return true;
+    } catch (error) {
+      console.log("AppWrite Service :: deletePost :: ", error);
+      return false;
+    }
+  }
+
+  //make service to find or get the post
+  async getPost(slug) {
+    try {
+      return await this.database.getDocument(
+        conf.appwriteDatabaseId,
+        conf.appwriteCollectionId,
+        slug
+      );
+    } catch (error) {
+      console.log("AppWrite Service :: getPost ::", error);
+      return false;
+    }
+  }
+
+  //make service for find or get the selected posts
+  async getPosts(queries = [Query.equal("status", "active")]) {
+    try {
+      return await this.database.listDocuments(
+        conf.appwriteDatabaseId,
+        conf.appwriteCollectionId,
+        queries
+      );
+    } catch (error) {
+      console.log("AppWrite Service :: getPosts ::", error);
+    }
+  }
+
+  //file upload service
+  async uploadFile(file) {
+    try {
+      return await this.bucket.createFile(
+        conf.appwriteBucketId,
+        ID.unique(),
+        file
+      );
+    } catch (error) {
+      console.log("AppWrite Service :: uploadFile ::", error);
+      return false;
+    }
+  }
+
+  //file delete service
+  async deleteFile(fileId) {
+    try {
+      return await this.bucket.deleteFile(conf.appwriteBucketId, fileId);
+      return true;
+    } catch (error) {
+      console.log("AppWrite Service :: deleteFile ::", error);
+      return false;
+    }
+  }
+
+  //make service for getFilePreview
+  getFilePreview(fileId) {
+    return this.bucket.getFilePreview(conf.appwriteBucketId, fileId);
+  }
 }
 
-const service = new Service();
+const service = new Service(); //this object or instace is crated for Service class and we will use the class methods using dot operator
